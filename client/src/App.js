d0eac7c660553f71d95c523a8848f71fff87e538
Sisir <das1234sisir@gmai.com>
2023-08-24 19:49:35 +0530

    Router n Tailwind added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Main, Login } from "./containiers";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+      <Routes>
+        <Route path="/*" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/containiers/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className=" w-screen h-screen relative overflow-hidden flex">
+      {/*  BackGround Image */}
+    </div>
+  );
+};
+
+export default Login;
